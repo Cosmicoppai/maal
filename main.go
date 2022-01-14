fcdf980b65b91e298eb5aeca7fc1d0e1c93dc12e
CosmicOppai <kanakchaudhari12@gmail.com>
2022-01-14 18:16:47 +0530

    added the code inside the forever loop

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 )
 
-// anime-name ep_no sub/dub
+// anime-name ep_no  -> Input Format
 
 func HandleReadingError(err error, issueIn string) {
 	if err != nil {
@@ -21,56 +21,57 @@ func HandleReadingError(err error, issueIn string) {
 var rootDomain = "https://ww1.gogoanime2.org"
 
 func main() {
-	arguments := os.Args
-	var animeName, epNo string
-	if len(arguments) <= 2 {
+	for {
+		var animeName, epNo string
+
 		fmt.Println("Enter Anime-Name Episode-Number in \"name ep-no\" format ")
-		fmt.Println("Example: (shingeki-no-kyojin 1) (shingeki-no-kyojin-dub 1), for movies enter 1 for episode")
+		fmt.Println("Example: (shingeki-no-kyojin 1) (shingeki-no-kyojin-dub 1), for movies enter 1 for episode (kimi-no-na-wa 1)")
 		_, err := fmt.Scan(&animeName)
 		HandleReadingError(err, "Anime-Name")
 		_, err = fmt.Scan(&epNo)
 		HandleReadingError(err, "Episode-Number")
-	} else {
-		animeName = arguments[1]
-		epNo = arguments[2]
-	}
-	fmt.Printf("Playing %s episode %s \n", animeName, epNo)
-	client := &http.Client{}
-	request, err := http.NewRequest("GET", fmt.Sprintf("%s/watch/%s/%s", rootDomain, animeName, epNo), nil)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	request.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36")
-	resp, err := client.Do(request)
-	if err != nil {
-		log.Println("Error while making the request", err)
-		return
-	}
-	if resp.StatusCode == 200 {
-		// _, err = io.Copy(os.Stdout, resp.Body)
-		dataInBytes, err := ioutil.ReadAll(resp.Body)
+
+		fmt.Printf("Playing %s episode %s \n", animeName, epNo)
+
+		client := &http.Client{}
+		request, err := http.NewRequest("GET", fmt.Sprintf("%s/watch/%s/%s", rootDomain, animeName, epNo), nil)
 		if err != nil {
-			log.Println("Error while Reading the Body: ", err)
+			log.Println(err)
 			return
 		}
-		content := string(dataInBytes)
-		iframeStartIndex := strings.Index(content, "<iframe id=\"playerframe\" src=")
-		if iframeStartIndex == -1 {
-			fmt.Println(content, iframeStartIndex)
-			log.Println("Video src doesn't exist")
+		request.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36")
+		resp, err := client.Do(request)
+		if err != nil {
+			log.Println("Error while making the request", err)
 			return
 		}
-		index := iframeStartIndex + 30
-		src := rootDomain + content[index:index+15]
-		fmt.Println(src)
-		cmd := exec.Command("T://bootstrapper/mpv.exe", src)
-		// cmd.Path = "T://bootstrapper"
-		cmd.Stderr = os.Stderr
-		if err := cmd.Run(); err != nil {
-			log.Println("Error while playing the video: ", err)
-			return
+		if resp.StatusCode == 200 {
+			// _, err = io.Copy(os.Stdout, resp.Body)
+			dataInBytes, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Println("Error while Reading the Body: ", err)
+				return
+			}
+			content := string(dataInBytes)
+			iframeStartIndex := strings.Index(content, "<iframe id=\"playerframe\" src=")
+			if iframeStartIndex == -1 {
+				fmt.Println(content, iframeStartIndex)
+				log.Println("Video src doesn't exist")
+				return
+			}
+			startIndex := iframeStartIndex + 30
+			content = content[startIndex:]
+			endIndex := strings.Index(content, "\" style=\"width: 100%;\"")
+			src := rootDomain + content[:endIndex]
+			fmt.Println("Link Collected: ", src)
+			cmd := exec.Command("T://bootstrapper/mpv.exe", src)
+			cmd.Stderr = os.Stderr
+			if err := cmd.Run(); err != nil {
+				log.Println("Error while playing the video: ", err)
+				return
+			}
+		} else {
+			log.Println("Anime Doesn't exist, Recheck all the information!")
 		}
 	}
-	log.Println("Anime Doesn't exist, Recheck all the information!")
 }
